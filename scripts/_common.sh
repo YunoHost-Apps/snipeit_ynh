297579c1930432e3d0c21e53f254f65fc3c01a9d
ericgaspar <junk.eg@free.fr>
2021-11-14 12:57:53 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # Packages requirements: https://snipe-it.readme.io/docs/requirements
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="7.3"
 
 pkg_dependencies="openssl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd"
 

7e693987ef4f2c8d0ab7b41aafe885ff28dfb5d2
ericgaspar <junk.eg@free.fr>
2021-11-14 12:43:43 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,9 @@
 
 # Packages requirements: https://snipe-it.readme.io/docs/requirements
 
-YNH_PHP_VERSION=7.4
+YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="openssl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-gd"
+pkg_dependencies="openssl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd"
 
 YNH_COMPOSER_VERSION=2.0.14
 

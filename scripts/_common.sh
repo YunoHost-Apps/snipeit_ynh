1285dd280f0c47e689bc38cff7b4059bb040d397
yalh76 <yalh@yahoo.com>
2022-06-30 21:06:02 +0200

    Apply example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,10 +7,10 @@
 # Packages requirements: https://snipe-it.readme.io/docs/requirements
 
 YNH_PHP_VERSION="7.3"
-
-pkg_dependencies="openssl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd"
-
 YNH_COMPOSER_VERSION=2.0.14
+
+# dependencies used by the app
+pkg_dependencies="openssl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd"
 
 #=================================================
 # PERSONAL HELPERS

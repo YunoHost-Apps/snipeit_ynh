0481d83c33bc539d557e8870fa06be0bbddde910
Tagada <36127788+Tagadda@users.noreply.github.com>
2023-06-13 02:43:43 +0200

    Update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,7 @@
 # Packages requirements: https://snipe-it.readme.io/docs/requirements
 
 YNH_COMPOSER_VERSION=2.5.8
+timezone=$(cat /etc/timezone)
 
 #=================================================
 # PERSONAL HELPERS

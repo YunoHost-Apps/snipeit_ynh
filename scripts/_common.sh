f64e12bb31e81312f29aa3bcdcf3c106efc790f6
eric_G <46165813+ericgaspar@users.noreply.github.com>
2024-09-11 11:32:25 +0200

    Testing (#72)
    * Upgrade to v7.0.10 (#63)
    
    * Upgrade sources
    - `main` v7.0.10: https://github.com/snipe/snipe-it/releases/tag/7.0.10
    
    * Auto-update READMEs
    
    * Update manifest.toml (#65)
    
    * Upgrade to v7.0.11 (#67)
    
    * Upgrade sources
    - `main` v7.0.11: https://github.com/snipe/snipe-it/releases/tag/7.0.11
    
    * Auto-update READMEs
    
    * Upgrade to v7.0.12 (#70)
    
    * Upgrade sources
    - `main` v7.0.12: https://github.com/snipe/snipe-it/releases/tag/7.0.12
    
    * Auto-update READMEs
    
    * [autopatch] Automatic patch attempt for helpers 2.1 (#69)
    
    * Testing (#68)
    
    * Upgrade to v7.0.10 (#63)
    
    * Upgrade sources
    - `main` v7.0.10: https://github.com/snipe/snipe-it/releases/tag/7.0.10
    
    * Auto-update READMEs
    
    * Update manifest.toml (#65)
    
    * Upgrade to v7.0.11 (#67)
    
    * Upgrade sources
    - `main` v7.0.11: https://github.com/snipe/snipe-it/releases/tag/7.0.11
    
    * Auto-update READMEs
    
    ---------
    
    Co-authored-by: YunoHost Bot <yunohost-bot@users.noreply.github.com>
    
    * [autopatch] Automatic patch attempt for helpers 2.1
    
    * cleaning
    
    ---------
    
    Co-authored-by: eric_G <46165813+ericgaspar@users.noreply.github.com>
    Co-authored-by: Yunohost-Bot <>
    
    * Update install
    
    ---------
    
    Co-authored-by: YunoHost Bot <yunohost-bot@users.noreply.github.com>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 # Packages requirements: https://snipe-it.readme.io/docs/requirements
 
-YNH_COMPOSER_VERSION=2.5.8
+composer_version=2.5.8
 
 timezone=$(cat /etc/timezone)
-
-#=================================================
-# PERSONAL HELPERS
-#=================================================
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
